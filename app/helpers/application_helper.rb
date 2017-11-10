74d87de1ba4322317d31c69a1e319931498e174f
nhatquang <vonhatquang100@gmail.com>
2017-11-10 15:19:51 +0700

    CHuong 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def hello
+    render html: "hello, world!"
+  end
+  def full_title page_title = ""
+    base_title = t".tieude"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
